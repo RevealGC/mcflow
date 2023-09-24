602d9919eb13fb3b2fcd2f7847aa6543f52c1a1a
mahesh-kariya <mahesh.kariya@revealgc.com>
2023-09-24 21:31:35 +0530

    Init mcflow

---
+++ script.sh
@@ -0,0 +1,13 @@
+REPO_URL="https://github.com/RevealGC/mcflow.git"
+DIR_NAME="mcflow"
+
+# Check if the directory exists
+if [ -d "$DIR_NAME" ]; then
+    echo "Directory '$DIR_NAME' already exists. Performing a pull..."
+    cd "$DIR_NAME"
+    git pull origin main
+else
+    echo "Directory '$DIR_NAME' does not exist. Cloning the repository..."
+    git clone "$REPO_URL"
+    cd "$DIR_NAME"
+fi
